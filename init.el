3922c5e14553b66f0a0b075cb7ec348345fb4709
Wladimir Labeikovsky <wladimir@maximumgoldfish.net>
2023-08-16 00:07:58 +0200

    Add init.el with test minimal settings

---
+++ init.el
@@ -0,0 +1,14 @@
+;; Running on macOS Ventura
+;; GNU Emacs 29.1 Yamamoto Mitsuharu build
+;;
+;; compiled with: --with-starter
+;;                --with-native-comp
+;;                --with-xwidgets
+;;                --with-imagemagick
+;;                --with-librsvg
+
+;; Don't show the splash screen
+(setq inhibit-startup-message t)
+
+;; Load dark Modus theme
+(load-theme 'modus-vivendi t)
